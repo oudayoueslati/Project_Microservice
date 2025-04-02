a2c3610002c17cff76f8b4e3af1f94562165206b
oudayoueslati <147772540+oudayoueslati@users.noreply.github.com>
2025-04-02 18:32:33 +0100

    mise_a_jour

--- offre_promotion/src/main/java/tn/esprit/offre_promotion/OffrePromotionApplication.java
+++ offre_promotion/src/main/java/tn/esprit/offre_promotion/OffrePromotionApplication.java
@@ -3,7 +3,7 @@ package tn.esprit.offre_promotion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
